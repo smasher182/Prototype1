d24dc9d6315384c6b805703b42f56389da3a67e6
smasher182 <81832820+smasher182@users.noreply.github.com>
2022-07-03 05:28:49 +1000

    Updated player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,11 +8,15 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     // reference to rigid body component applied to the player.
     private Rigidbody playerRb;
+    // reference to focalPoint.
+    private GameObject focalPoint;
 
     void Start()
     {
         // initializes rigidbody component at start.
-        playerRb = GetComponent<Rigidbody>();       
+        playerRb = GetComponent<Rigidbody>();
+        // inititalizes focal point check at start.
+        focalPoint = GameObject.Find("Focal Point");
     }
 
     
@@ -21,6 +25,9 @@ public class PlayerController : MonoBehaviour
         // gets the player vertical input.
         float forwardInput = Input.GetAxis("Vertical");
         // moves the player with AddForce method based on forwardInput.
-        playerRb.AddForce(Vector3.forward * speed * forwardInput);
+        //playerRb.AddForce(Vector3.forward * speed * forwardInput);
+
+        // moves the player in the direction of focal point with AddForce method based on forwardInput. 
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
     }
 }
